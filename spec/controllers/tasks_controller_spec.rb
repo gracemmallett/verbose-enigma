2d4311c564f872cc743fc5e95d2313268fefe048
Grace Mallett <gmmallet@bowdoin.edu>
2017-05-31 22:08:17 +0000

    toggling

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,7 +10,16 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+    end
+  end
 
+    describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryGirl.create(:task, done: false)
+      put :update, id: task.id, task: { done: true }
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
     end
   end
 
